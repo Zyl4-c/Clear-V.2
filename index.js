fa5c8f80d16f1fae56c68cbdce7f570ddc2988f7
Peerapat tachacharoensuk <119061962+Zyl4-c@users.noreply.github.com>
2022-12-03 15:11:03 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,140 @@
+const { DisTube } = require('distube')
+const Discord = require('discord.js')
+const client = new Discord.Client({
+  partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  intents: [
+    Discord.GatewayIntentBits.Guilds,
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.GuildVoiceStates,
+    Discord.GatewayIntentBits.MessageContent
+  ]
+});
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+const fs = require('fs')
+const config = require('./config.json')
+const { SpotifyPlugin } = require('@distube/spotify')
+const { SoundCloudPlugin } = require('@distube/soundcloud')
+const { YtDlpPlugin } = require('@distube/yt-dlp')
+
+client.config = require('./config.json')
+client.distube = new DisTube(client, {
+  leaveOnStop: false,
+  emitNewSongOnly: true,
+  emitAddSongWhenCreatingQueue: false,
+  emitAddListWhenCreatingQueue: false,
+  plugins: [
+    new SpotifyPlugin({
+      emitEventsAfterFetching: true
+    }),
+    new SoundCloudPlugin(),
+    new YtDlpPlugin()
+  ]
+})
+client.commands = new Discord.Collection()
+client.aliases = new Discord.Collection()
+client.emotes = config.emoji
+
+fs.readdir('./commands/', (err, files) => {
+  if (err) return console.log('Could not find any commands!')
+  const jsFiles = files.filter(f => f.split('.').pop() === 'js')
+  if (jsFiles.length <= 0) return console.log('Could not find any commands!')
+  jsFiles.forEach(file => {
+    const cmd = require(`./commands/${file}`)
+    console.log(`Loaded ${file}`)
+    client.commands.set(cmd.name, cmd)
+    if (cmd.aliases) cmd.aliases.forEach(alias => client.aliases.set(alias, cmd.name))
+  })
+})
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} พร้อมทำงานแล้ว ~ ~`)
+})
+
+client.on('messageCreate', async message => {
+  if (message.author.bot || !message.guild) return
+  const prefix = config.prefix
+  if (!message.content.startsWith(prefix)) return
+  const args = message.content.slice(prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
+  const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+  if (!cmd) return
+  if (cmd.inVoiceChannel && !message.member.voice.channel) {
+    return message.channel.send(`${client.emotes.error} | คุณต้องอยู่ในห้องพูดคุยก่อนน้า`)
+  }
+  try {
+    cmd.run(client, message, args)
+  } catch (e) {
+    console.error(e)
+    message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
+  }
+})
+
+const status = queue =>
+  `เสียง: \`${queue.volume}%\`| ฟิลเตอร์: \`${queue.filters.names.join(', ') || 'ปิด'}\`| วนเพลง: \`${
+    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'ปิด'
+  }\` | เล่นไปเรื่อย: \`${queue.autoplay ? 'เปิด' : 'ปิด'}\``
+
+client.distube
+  .on('playSong', (queue, song) =>
+    queue.textChannel.send(
+      `${client.emotes.play} |  กำลังเล่น \`${song.name}\` - \`${song.formattedDuration}\`
+      ${status(queue)}`
+//  ผู้ใช้คำสั่ง: ${
+//        song.user
+//      }`
+      
+    )
+  )
+  .on('addSong', (queue, song) =>
+    queue.textChannel.send(
+      `เพิ่มเพลง ${song.name} - \`${song.formattedDuration}\`\nไปที่คิวโดย ${song.user}`
+    )
+  )
+  .on('addList', (queue, playlist) =>
+    queue.textChannel.send(
+      `${client.emotes.success} | เพิ่ม \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs) ไปที่คิว\n${status(queue)}`
+    )
+  )
+  .on('error', (channel, e) => {
+    if (channel) channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
+    else console.error(e)
+  })
+  .on('empty', channel => channel.send('ไม่มีใครอยู่แล้วอ่า ~ ~ เคลียร์ไปแล้วนะ :3'))
+  .on('searchNoResult', (message, query) =>
+    message.channel.send(`${client.emotes.error} | ไม่เห็นเจออารายเลยยย ~ ~ \`${query}\`!`)
+  )
+  .on('finish', queue => queue.textChannel.send('เพลงหมดแย้วค่าาาา ~ ~'))
+  
+  client.login(config.token)
+// // DisTubeOptions.searchSongs = true
+// .on("searchResult", (message, result) => {
+//     let i = 0
+//     message.channel.send(
+//         `**Choose an option from below**\n${result
+//             .map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``)
+//             .join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`
+//     )
+// })
+// .on("searchCancel", message => message.channel.send(`${client.emotes.error} | Searching canceled`))
+// .on("searchInvalidAnswer", message =>
+//     message.channel.send(
+//         `${client.emotes.error} | Invalid answer! You have to enter the number in the range of the results`
+//     )
+// )
+// .on("searchDone", () => {})
+
+
